ea4bb31c823ff39dbfd727df8d9967b837e3510b
skikhtear <skikhtear@gmail.com>
2022-10-27 02:58:55 +0600

    blog q4

--- src/Page/Blog/Blog.js
+++ src/Page/Blog/Blog.js
@@ -27,6 +27,20 @@ const Blog = () => {
                     he will be redirected to the login page and the user can only access the
                     authenticated routes If he is authenticated (Logged in).</p>
             </div>
+            <div>
+                <h1>What is Node? How does Node work?</h1>
+                <p>Javascript has existed since 1995 and has since taken over as the
+                    dominant language for web development. For much of its life, JavaScript
+                    was used mainly for client-side scripting inside tags executing
+                        in web browsers. This limitation meant that developers were often working
+                        in many different languages and frameworks between the front-end (client-side)
+                    and backend (server-side) aspects of a web application.</p>
+                <p>Node provides access to several important global objects for use with Node program files. When writing a file that will run in a Node environment, these variables will be accessible in the global scope of your file.
+
+                    module is an object referring to the functionality that will be exported from a file. In Node, each file is treated as a module.
+                    require() is a function used to import modules from other files or Node packages.
+                    process is an object referencing to the actual computer process running a Node program and allows for access to command-line arguments and much more.</p>
+            </div>
         </div>
     );
 };
